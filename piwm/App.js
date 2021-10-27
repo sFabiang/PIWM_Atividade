930bb1fd33ea1f107b59ddc763be68c16d36ea09
Samuel Fabián <sfabian1996@gmail.com>
2021-10-27 18:19:52 -0300

    Atividade concluída

--- piwm/App.js
+++ piwm/App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Cabecalho from './views/Cabecalho.jsx';
+import Disciplina from './views/Disciplina.jsx';
+import Corpo from './views/Corpo.jsx';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Corpo/>
+      <Cabecalho nome='Samuel Fabián' curso='Design Digital'/>
+      <br/>
+      <Text style={{fontWeight: "bold"}}>Disciplinas matriculadas:</Text>
+      <ul>
+        <li><Disciplina disciplina='PIWM'/></li>
+        <li><Disciplina disciplina='Sociedade, Cultura e Tecnologia'/></li>
+        <li><Disciplina disciplina='Design e Inovação'/></li>
+        <li><Disciplina disciplina='User Experience (UX)'/></li>
+        <li><Disciplina disciplina='Estágio I'/></li>
+      </ul>
+      
+      
     </View>
   );
 }
@@ -17,5 +31,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
